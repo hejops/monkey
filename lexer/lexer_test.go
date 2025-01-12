8e8bbc0a8e606ef69ed7c62437126179cad20438
Joseph <hejops1@gmail.com>
2025-01-12 18:31:03 +0100

    test: test function definition and function call

--- lexer/lexer_test.go
+++ lexer/lexer_test.go
@@ -54,28 +54,65 @@ func TestSingleAssignment(t *testing.T) {
 	}
 }
 
-// func TestBasicProgram(t *testing.T) {
-// 	lexer := New(`let five = 5;
-// let ten = 10;
-// let add = fn(x, y) {
-// 	x + y;
-// };
-// let result = add(five, ten);
-// `)
-//
-// 	tokens := []struct {
-// 		_type token.TokenType
-// 		s     string
-// 	}{
-// 		{token.LET, "="},
-// 		{token.IDENT, "five"},
-// 		{token.ASSIGN, "="},
-// 		{token.INT, "5"},
-// 	}
-//
-// 	for _, tt := range tokens {
-// 		tok := lexer.NextToken()
-// 		assert.Equal(t, tok.Type, tt._type)
-// 		assert.Equal(t, tok.Literal, tt.s)
-// 	}
-// }
+func TestBasicProgram(t *testing.T) {
+	lexer := New(`let five = 5;
+let ten = 10;
+let add = fn(x, y) {
+	x + y;
+};
+let result = add(five, ten);
+`)
+
+	tokens := []struct { // {{{
+		_type token.TokenType
+		s     string
+	}{
+		{token.LET, "let"},
+		{token.IDENT, "five"},
+		{token.ASSIGN, "="},
+		{token.INT, "5"},
+		{token.SEMICOLON, ";"},
+
+		{token.LET, "let"},
+		{token.IDENT, "ten"},
+		{token.ASSIGN, "="},
+		{token.INT, "10"},
+		{token.SEMICOLON, ";"},
+
+		{token.LET, "let"},
+		{token.IDENT, "add"},
+		{token.ASSIGN, "="},
+		{token.FUNCTION, "fn"},
+		{token.LPAREN, "("},
+		{token.IDENT, "x"},
+		{token.COMMA, ","},
+		{token.IDENT, "y"},
+		{token.RPAREN, ")"},
+		{token.LBRACE, "{"},
+		{token.IDENT, "x"},
+		{token.PLUS, "+"},
+		{token.IDENT, "y"},
+		{token.SEMICOLON, ";"},
+		{token.RBRACE, "}"},
+		{token.SEMICOLON, ";"},
+
+		{token.LET, "let"},
+		{token.IDENT, "result"},
+		{token.ASSIGN, "="},
+		{token.IDENT, "add"},
+		{token.LPAREN, "("},
+		{token.IDENT, "five"},
+		{token.COMMA, ","},
+		{token.IDENT, "ten"},
+		{token.RPAREN, ")"},
+		{token.SEMICOLON, ";"},
+
+		{token.EOF, ""},
+	} // }}}
+
+	for _, tt := range tokens {
+		tok := lexer.NextToken()
+		assert.Equal(t, tok.Type, tt._type)
+		assert.Equal(t, tok.Literal, tt.s)
+	}
+}
